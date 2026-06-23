9e87d1109b3c03f4f7a74d6173925b69b68051c3
dweep-desai <dweep.vd@gmail.com>
2026-06-24 00:56:39 +0400

    fix v1.0.1 SHA256 after rebuilding DMG with correct version

--- Casks/facegate.rb
+++ Casks/facegate.rb
@@ -1,6 +1,6 @@
 cask "facegate" do
   version "1.0.1"
-  sha256 "1d2e4f38b7ee5cdbd4d71af657d8a0aa20691b9038aa5110639a4c2c800c54b4"
+  sha256 "f9e8d388acff5e099a6faeb05dbbfc764018bd20a791185dea4e658bafc739c2"
 
   url "https://github.com/dweep-desai/FaceGate-Mac/releases/download/v#{version}/FaceGate-#{version}.dmg"
   name "FaceGate"
