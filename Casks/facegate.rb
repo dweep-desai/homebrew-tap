23553497af11656a13eabb4ab8f7dc51afda15d1
dweep-desai <dweep.vd@gmail.com>
2026-06-23 21:57:25 +0400

    update facegate to v1.0.1

--- Casks/facegate.rb
+++ Casks/facegate.rb
@@ -1,6 +1,6 @@
 cask "facegate" do
-  version "1.0.0"
-  sha256 "814f0f7e9a9633acc870ea51a655afdcf2413f79fce6b1dea7b7d035b0a6285c"
+  version "1.0.1"
+  sha256 "1d2e4f38b7ee5cdbd4d71af657d8a0aa20691b9038aa5110639a4c2c800c54b4"
 
   url "https://github.com/dweep-desai/FaceGate-Mac/releases/download/v#{version}/FaceGate-#{version}.dmg"
   name "FaceGate"
