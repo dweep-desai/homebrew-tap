a4e8932aef2204439bea10b62956a87b9190953b
dweep-desai <dweep.vd@gmail.com>
2026-06-24 01:08:39 +0400

    fix v1.0.1 DMG: About page now reads version from bundle

--- Casks/facegate.rb
+++ Casks/facegate.rb
@@ -1,6 +1,6 @@
 cask "facegate" do
   version "1.0.1"
-  sha256 "f9e8d388acff5e099a6faeb05dbbfc764018bd20a791185dea4e658bafc739c2"
+  sha256 "325605ed0a5577d6fb5e70c9c92ee9f2424e94214b89f9ee47c5aa891eb8a80e"
 
   url "https://github.com/dweep-desai/FaceGate-Mac/releases/download/v#{version}/FaceGate-#{version}.dmg"
   name "FaceGate"
