9ae52a42c4538ab6fe8b897433c0d7855e03ae82
dweep-desai <dweep.vd@gmail.com>
2026-07-01 20:13:08 +0400

    Update lensss to v1.0.0 and add sha256 checksum

--- Casks/lensss.rb
+++ Casks/lensss.rb
@@ -1,8 +1,8 @@
 cask "lensss" do
   version "1.0.0"
-  sha256 :no_check
+  sha256 "8b36364db776be41f5b7945410a82446a82293d3f35de9a0078e49d96f89023a"
 
-  url "https://github.com/dweep-desai/lensss/releases/download/v#{version}/Lensss.dmg"
+  url "https://github.com/dweep-desai/lensss/releases/download/v#{version}/lensss-#{version}.dmg"
   name "Lensss"
   desc "macOS menu bar utility to search anything with Google Lens"
   homepage "https://github.com/dweep-desai/lensss"
